a90c02c80437a6b274e44cfda8f2ee8a4657ee91
Sam-Mobed <masdebom@gmail.com>
2023-05-05 10:47:51 -0400

    finished parallax effect

--- src/Mountain.js
+++ src/Mountain.js
@@ -3,9 +3,18 @@ import './Mountain.css';
 import hafez1 from './images/hafez1.jpg';
 import mountain from './images/mountain.png';
 import hill from './images/hill.png';
+import { useEffect } from 'react';
 
 function Mountain() {
-  return (
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            let scrollY = window.scrollY;
+            document.documentElement.style.setProperty("--scrollTop", `${scrollY}px`);
+        });
+    }, []);
+
+    return (
     <div className="parallax-container">
         <div className="content">
             <header className="header-main">
@@ -13,11 +22,14 @@ function Mountain() {
                     <div className="layer-head">
                         <h1 className="title">McGill Iranian Student Association</h1>
                     </div>
+                    <div className="img-layer layer-base" style={{backgroundImage: `url(${hafez1})`, backgroundSize: "100% auto",}}></div>
+                    <div className="img-layer layer-mid" style={{backgroundImage: `url(${mountain})`, backgroundSize: "100% auto", backgroundRepeat: "no-repeat", bottom:"0",}}></div>
+                    <div className="img-layer layer-front" style={{backgroundImage: `url(${hill})`, backgroundSize: "100% auto", backgroundRepeat: "no-repeat",}}></div>
                 </div>
             </header>
         </div>
     </div>
-  );
+    );
 }
 //<h1 class="site-title">McGill Iranian Student Association</h1>
 export default Mountain;
