01a6f4b95746704869223589abb0516e7c7a945b
Sam-Mobed <masdebom@gmail.com>
2023-05-06 11:37:20 -0400

    started working on the title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,4 +1,7 @@
 import './Title.css';
+import cloud from './images/cloud.png';
+import blue_clouds from './images/Blue_Clouds.png';
+import hanging_plants from './images/plant_corner.png';
 
 function Title() {
 
@@ -6,9 +9,19 @@ function Title() {
 
     return (
         <div className="title-container">
+            <div className="row1">
+                <div className="img cloud" style={{backgroundImage: `url(${cloud})`,}}></div>
+                <div className="img blue_clouds" style={{backgroundImage: `url(${blue_clouds})`,}}></div>
+                <div className="img hanging_platns" style={{backgroundImage: `url(${hanging_plants})`,}}></div>
+            </div>
+            <div className="row2">
+                <h1 className="title">McGill Iranian Student Association</h1>
+            </div>
+            <div className="row3"></div>
             
-            <h1 className="title">McGill Iranian Student Association</h1>
+            
         </div>
+        
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Mountain />
     <Title />
-    <App />
+    
   </React.StrictMode>
 );
 
